89b5e4ae3d8cccce10ae29c12dcad6a3bc321a22
DaniloPV18 <damipive1809@gmail.com>
2023-03-07 22:26:31 -0500

    Modelos movidos al proyecto API

---
+++ CoffeStore.SolucionBack/CoffeStore.APIs/Models/FormaPago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeStore.APIs.Models
+{
+    public class FormaPago
+    {
+        public string? Codigo { get; set; }
+        public string? Metodo { get; set; }
+    }
+}
---
+++ CoffeStore.SolucionBack/CoffeStore.APIs/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlTypes;
+
+namespace CoffeStore.APIs.Models
+{
+    public class Usuario
+    {
+        public int? Id { set; get; }
+        public string? Cedula { set; get; }
+        public string? Nombres { set; get; }
+        public string? Apellidos { set; get; }
+        public SqlDateTime? FechaNacimiento { set; get; }
+        public string? Email { set; get; }
+        public string? Contrasena { set; get; }
+        public RolUsuario? Rol { set; get; }
+        public string? Estado { set; get; }
+        public SqlDateTime? CreatedAt { set; get; }
+        public SqlDateTime? UpdatedAt { set; get; }
+        public string Transaccion { get; set; }
+    }
+}
