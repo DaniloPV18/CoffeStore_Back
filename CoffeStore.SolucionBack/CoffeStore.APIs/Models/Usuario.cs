ead6d88aeb9854e5da11f6591a987e03cd79a3f5
Keneth Riera <kenethrr@outlook.com>
2023-03-08 21:34:03 -0500

    Se actualizan métodos relacionados a la seguridad

--- CoffeStore.SolucionBack/CoffeStore.APIs/Models/Usuario.cs
+++ CoffeStore.SolucionBack/CoffeStore.APIs/Models/Usuario.cs
@@ -4,14 +4,14 @@ namespace CoffeStore.APIs.Models
 {
     public class Usuario
     {
-        public int? Id { set; get; }
+        public int Id { set; get; }
         public string? Cedula { set; get; }
-        public string? Nombres { set; get; }
-        public string? Apellidos { set; get; }
+        public string Nombres { set; get; }
+        public string Apellidos { set; get; }
         public SqlDateTime? FechaNacimiento { set; get; }
         public string? Email { set; get; }
         public string? Contrasena { set; get; }
-        public RolUsuario? Rol { set; get; }
+        public RolUsuario Rol { set; get; }
         public string? Estado { set; get; }
         public SqlDateTime? CreatedAt { set; get; }
         public SqlDateTime? UpdatedAt { set; get; }
---
+++ CoffeStore.SolucionBack/CoffeStore.APIs/Utils/SecurityUtils.cs
@@ -0,0 +1,115 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using CoffeStore.APIs.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
+
+namespace CoffeStore.APIs.Utils;
+
+public class SecurityUtils
+{
+    public static string GenerateJWTToken(Usuario user, string secretKey)
+    {
+        if (user == null)
+        {
+            throw new ArgumentNullException("user");
+        }
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim("nombres", user.Nombres),
+                new Claim("apellidos", user.Apellidos),
+                new Claim("rol", user.Rol.Codigo)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public static bool ValidateJWTToken(string token, string secretKey)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        tokenHandler.ValidateToken(token, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        }, out SecurityToken validatedToken);
+
+        return validatedToken != null;
+    }
+
+    public static string HashPassword(string passwordToEncrypt)
+    {
+        byte[] salt;
+        byte[] buffer2;
+        if (passwordToEncrypt == null)
+        {
+            throw new ArgumentNullException("password");
+        }
+        using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(passwordToEncrypt, 0x10, 0x3e8))
+        {
+            salt = bytes.Salt;
+            buffer2 = bytes.GetBytes(0x20);
+        }
+        byte[] dst = new byte[0x31];
+        Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+        Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+        return Convert.ToBase64String(dst);
+    }
+
+    public static bool VerifyHashedPassword(string hashedPassword, string password)
+    {
+        byte[] buffer4;
+        if (hashedPassword == null)
+        {
+            return false;
+        }
+        if (password == null)
+        {
+            throw new ArgumentNullException("password");
+        }
+        byte[] src = Convert.FromBase64String(hashedPassword);
+        if ((src.Length != 0x31) || (src[0] != 0))
+        {
+            return false;
+        }
+        byte[] dst = new byte[0x10];
+        Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+        byte[] buffer3 = new byte[0x20];
+        Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+        using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+        {
+            buffer4 = bytes.GetBytes(0x20);
+        }
+        return ByteArraysEqual(buffer3, buffer4);
+    }
+
+    private static bool ByteArraysEqual(byte[] b1, byte[] b2)
+    {
+        if (b1 == b2) return true;
+        if (b1 == null || b2 == null) return false;
+        if (b1.Length != b2.Length) return false;
+        for (int i = 0; i < b1.Length; i++)
+        {
+            if (b1[i] != b2[i]) return false;
+        }
+        return true;
+    }
+}
