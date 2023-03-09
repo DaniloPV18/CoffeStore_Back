69460523dcdfea81ad92dc9237a10a3348da0e2d
Keneth Riera <kenethrr@outlook.com>
2023-03-08 22:20:52 -0500

    Se actualizan scripts de creación y población de datos

--- ScriptsBD/Create_Tables.sql
+++ ScriptsBD/Create_Tables.sql
@@ -1,77 +1,87 @@
-Create database Proyecto
+DROP DATABASE IF EXISTS Proyecto
 
-CREATE TABLE RolUsuario (
-    Codigo			varchar(20) primary key,
-    Descripcion		varchar(255)
+CREATE DATABASE Proyecto
+
+USE Proyecto
+
+CREATE TABLE RolUsuario
+(
+	Codigo varchar(20) primary key,
+	Descripcion varchar(255)
 );
 
-CREATE TABLE Usuario (
-    Id				int identity(1,1) primary key,
-    Nombres			varchar(255),
-    Apellidos		varchar(255),
-    FechaNacimiento date,
-    Email			varchar(50),
-	Contrasena		varchar(50),
-	Rol				varchar(20),
-	Estado			varchar(50),
-	CreatedAt		datetime,
-	UpdateAt		datetime,
+CREATE TABLE Usuario
+(
+	Id int identity(1,1) primary key,
+	Nombres varchar(255),
+	Apellidos varchar(255),
+	FechaNacimiento date,
+	Email varchar(50),
+	Contrasena varchar(50),
+	Rol varchar(20),
+	Estado varchar(50),
+	CreatedAt datetime,
+	UpdateAt datetime,
 	FOREIGN KEY (Rol) REFERENCES RolUsuario(Codigo),
 );
 
-CREATE TABLE FormaPago (
-	Codigo			varchar(20) primary key,
-	Metodo			varchar(50)
+CREATE TABLE FormaPago
+(
+	Codigo varchar(20) primary key,
+	Metodo varchar(50)
 );
 
-CREATE TABLE FormaPago_Usuario (
-	Id				int identity(1,1) primary key,
-	Usuario			int,
-	FormaPago		varchar(20),
+CREATE TABLE FormaPago_Usuario
+(
+	Id int identity(1,1) primary key,
+	Usuario int,
+	FormaPago varchar(20),
 
-	Cuenta			varchar(50),
-	CreatedAt		datetime,
+	Cuenta varchar(50),
+	CreatedAt datetime,
 	--
 	FOREIGN KEY (FormaPago) REFERENCES FormaPago(Codigo),
-	FOREIGN KEY (Usuario)	REFERENCES Usuario(Id)	
+	FOREIGN KEY (Usuario)	REFERENCES Usuario(Id)
 );
 
 -- MEL
 
-CREATE TABLE CategoriaProducto (
-	Codigo           varchar(20) primary key,
-	Descripcion      varchar(255)
+CREATE TABLE CategoriaProducto
+(
+	Codigo varchar(20) primary key,
+	Descripcion varchar(255)
 );
 
-CREATE TABLE Producto (
-	Id				int identity(1,1) primary key,
-	Nombre			varchar(255),
-	Descripcion		varchar (255),
-	ImagenUrl		varchar (255),
-	Precio			float,
-	Categoria		varchar(20),
-	Estado			varchar(255),
-	CreatedAt		datetime,
-	UpdatedAt		datetime,
+CREATE TABLE Producto
+(
+	Id int identity(1,1) primary key,
+	Nombre varchar(255),
+	Descripcion varchar (255),
+	ImagenUrl varchar (255),
+	Precio float,
+	Categoria varchar(20),
+	Estado varchar(255),
+	CreatedAt datetime,
+	UpdatedAt datetime,
 
 	FOREIGN KEY (Categoria) REFERENCES CategoriaProducto(Codigo),
 );
 
-CREATE TABLE CompraUnidad (
-	Id						int identity(1,1) primary key,
-	Usuario					int,
-	FormaPago_Usuario		int,
-	Producto				int,
-	Cantidad				int,
+CREATE TABLE CompraUnidad
+(
+	Id int identity(1,1) primary key,
+	Producto int,
+	Cantidad int,
 
-	FOREIGN KEY (Usuario) REFERENCES Usuario (Id),
-	FOREIGN KEY (FormaPago_Usuario) REFERENCES FormaPago_Usuario (Id),
 	FOREIGN KEY (Producto) REFERENCES Producto (Id)
 );
 
-CREATE TABLE CompraConjunto (
-	Id                    int identity(1,1) primary key,
-	CompraUnidad          int,
+CREATE TABLE CompraConjunto
+(
+	Id int identity(1,1) primary key,
+	CompraUnidad int,
+	FormaPago_Usuario int,
 
+	FOREIGN KEY (FormaPago_Usuario) REFERENCES FormaPago_Usuario (Id),
 	FOREIGN KEY (CompraUnidad) REFERENCES CompraUnidad (Id)
 );
--- ScriptsBD/Insert_Data.sql
+++ ScriptsBD/Insert_Data.sql
@@ -1,73 +1,83 @@
-use Proyecto
-Go
--- Insert RolUsiuario
-INSERT INTO RolUsuario (Codigo, Descripcion)
-VALUES ('ADMIN', 'Administrador'),
-('USER', 'Usuario');
+USE Proyecto
+GO
+-- Insert RolUsuario
+INSERT INTO RolUsuario 
+    (Codigo, Descripcion)
+VALUES 
+    ('ADMIN', 'Administrador'),
+    ('USER', 'Usuario');
                
 -- Insert Usuario   
-INSERT INTO Usuario (Nombres, Apellidos, FechaNacimiento, Email, Contrasena, Rol, Estado, CreatedAt, UpdateAt)
-VALUES ('Ivonne', 'Minchala', '1998-12-25', 'Ivonne@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Keneth', 'Riera', '2000-12-02', 'Keneth@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Danilo', 'Pin', '1999-05-28 ', 'Danilo@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Melany', 'Mero', '1998-10-08', 'Melany@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Carlos', 'Bone', '1996-04-09', 'Carlos@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Bryan', 'Rizzo', '1999-07-30 ', 'Bryan@example.com', '1234', 'USER', 'activo', GETDATE(), null),
-('Roxana', 'Veloz', '1997-01-29', 'Roxana@example.com', '1234', 'ADMIN', 'activo', GETDATE(), null),
-('Jorge', 'Charco', '1986-08-19', 'Jorge@example.com', '1234', 'ADMIN', 'activo', GETDATE(), null);
+INSERT INTO Usuario 
+    (Nombres, Apellidos, FechaNacimiento, Email, Contrasena, Rol, Estado, CreatedAt, UpdateAt)
+VALUES 
+    ('Ivonne', 'Minchala', '1998-12-25', 'Ivonne@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Keneth', 'Riera', '2000-12-02', 'Keneth@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Danilo', 'Pin', '1999-05-28 ', 'Danilo@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Melany', 'Mero', '1998-10-08', 'Melany@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Carlos', 'Bone', '1996-04-09', 'Carlos@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Bryan', 'Rizzo', '1999-07-30 ', 'Bryan@example.com', '1234', 'USER', 'A', GETDATE(), null),
+    ('Roxana', 'Veloz', '1997-01-29', 'Roxana@example.com', '1234', 'ADMIN', 'A', GETDATE(), null),
+    ('Jorge', 'Charco', '1986-08-19', 'Jorge@example.com', '1234', 'ADMIN', 'A', GETDATE(), null);
 
 -- Insert Forma de Pago
-INSERT INTO FormaPago (Codigo, Metodo)
-VALUES ('EFE', 'Efectivo'),
-('DEB', 'Debito'),
-('PAY', 'PayPal'),
-('CRED', 'Credito');
+INSERT INTO FormaPago 
+    (Codigo, Metodo)
+VALUES 
+    ('EFE', 'Efectivo'),
+    ('DEB', 'Debito'),
+    ('PAY', 'PayPal'),
+    ('CRED', 'Credito');
 
 -- Insert Forma de Pago del Usuario 
-INSERT INTO FormaPago_Usuario (Usuario, FormaPago, Cuenta, CreatedAt)
-VALUES (1, 'PAY','payIvonne@Example', GETDATE()),
-(2, 'EFE',null, GETDATE()),
-(3, 'CRED','14563269854789652', GETDATE()),
-(4, 'DEB','12225698933654788', GETDATE()),
-(5, 'CRED','1477889633654526', GETDATE()),
-(6, 'EFE',null, GETDATE());
+INSERT INTO FormaPago_Usuario 
+    (Usuario, FormaPago, Cuenta, CreatedAt)
+VALUES 
+    (1, 'PAY', 'payIvonne@Example', GETDATE()),
+    (2, 'EFE', null, GETDATE()),
+    (3, 'CRED', '14563269854789652', GETDATE()),
+    (4, 'DEB', '12225698933654788', GETDATE()),
+    (5, 'CRED', '1477889633654526', GETDATE()),
+    (6, 'EFE', null, GETDATE());
 
 -- Insert categorias de Producto
-INSERT INTO CategoriaProducto (Codigo, Descripcion)
-VALUES ('BEBC', 'Bebida caliente'),
-('BEBF', 'Bebida  fría'),
-('Pos', 'Postre'),
-('SAN', 'Sándwiches');
+INSERT INTO CategoriaProducto 
+    (Codigo, Descripcion)
+VALUES 
+    ('BEBC', 'Bebida caliente'),
+    ('BEBF', 'Bebida  frÃ­a'),
+    ('Pos', 'Postre'),
+    ('SAN', 'Sï¿½ndwiches');
 
 -- Insert productos
-INSERT INTO Producto (Nombre, Descripcion, ImagenUrl, Precio, Categoria, Estado, CreatedAt, UpdatedAt)
-VALUES ('Capuchino', 'café espresso originaria de Italia', 'https://images.unsplash.com/photo-1534778101976-62847782c213?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 5.99, 'BEBC', 'activo', GETDATE(), null),
-('Milkshake', 'bebida espesa y cremosa hecha a base de helado batido con leche ', 'https://www.yourhomebasedmom.com/wp-content/uploads/2021/08/CHOCOLATE-MILK-SHAKE-5520.jpg',4.50, 'BEBF', 'activo', GETDATE(), null),
-('Cupcakes', 'pequeño pastel individual', 'https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 2.15, 'Pos', 'activo', GETDATE(), null),
-('frappés', 'bebida helada y espumosa hecha con hielo triturado, café expreso', 'https://img1.wsimg.com/isteam/ip/7b7b9caf-6f41-421f-ab42-97d07f7f833d/eiliv-sonas-aceron-44d0W8JhoPc-unsplash-0001.jpg/:/cr=t:17.53%25,l:17.53%25,w:64.94%25,h:64.94%25', 5.50, 'BEBF ', 'activo', GETDATE(), null),
-('sándwiches de jamón y queso', 'Dos rebanadas de pan con jamón y queso en el interior y Tostado', 'https://okdiario.com/img/2019/01/21/receta-de-sandwinch-de-jamon-y-queso-con-mostaza.jpg', 1.25, 'SAN', 'activo', GETDATE(), null),
-('Expreso', 'Dos rebanadas de pan con jamón y queso en el interior y Tostado', 'https://okdiario.com/img/2019/01/21/receta-de-sandwinch-de-jamon-y-queso-con-mostaza.jpg', 3.90, 'BEBC', 'activo', GETDATE(), null);
+INSERT INTO Producto 
+    (Nombre, Descripcion, ImagenUrl, Precio, Categoria, Estado, CreatedAt, UpdatedAt)
+VALUES 
+    ('Capuchino', 'cafÃ© espresso originaria de Italia', 'https://images.unsplash.com/photo-1534778101976-62847782c213?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 5.99, 'BEBC', 'A', GETDATE(), null),
+    ('Milkshake', 'bebida espesa y cremosa hecha a base de helado batido con leche ', 'https://www.yourhomebasedmom.com/wp-content/uploads/2021/08/CHOCOLATE-MILK-SHAKE-5520.jpg',4.50, 'BEBF', 'A', GETDATE(), null),
+    ('Cupcakes', 'pequeÃ±o pastel individual', 'https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 2.15, 'Pos', 'A', GETDATE(), null),
+    ('frappï¿½s', 'bebida helada y espumosa hecha con hielo triturado, cafÃ© expreso', 'https://img1.wsimg.com/isteam/ip/7b7b9caf-6f41-421f-ab42-97d07f7f833d/eiliv-sonas-aceron-44d0W8JhoPc-unsplash-0001.jpg/:/cr=t:17.53%25,l:17.53%25,w:64.94%25,h:64.94%25', 5.50, 'BEBF ', 'A', GETDATE(), null),
+    ('sï¿½ndwiches de jamÃ³n y queso', 'Dos rebanadas de pan con jamÃ³n y queso en el interior y Tostado', 'https://okdiario.com/img/2019/01/21/receta-de-sandwinch-de-jamon-y-queso-con-mostaza.jpg', 1.25, 'SAN', 'A', GETDATE(), null),
+    ('Expreso', 'Dos rebanadas de pan con jamï¿½n y queso en el interior y Tostado', 'https://okdiario.com/img/2019/01/21/receta-de-sandwinch-de-jamon-y-queso-con-mostaza.jpg', 3.90, 'BEBC', 'A', GETDATE(), null);
 
 -- insert Compra por Unidad 
-INSERT INTO CompraUnidad (Usuario, FormaPago_Usuario, Producto, Cantidad)
-VALUES (1, 11, 3, 2),
-(2, 12, 4, 1),
-(3, 13, 5, 4),
-(4, 14, 6, 3),
-(5, 15, 7, 2),
-(6, 16, 8, 3);
+INSERT INTO CompraUnidad 
+    (Producto, Cantidad)
+VALUES 
+    (1, 2),
+    (2, 1),
+    (3, 4),
+    (4, 3),
+    (5, 2),
+    (6, 3);
 
 -- Create purchase bundles
-INSERT INTO CompraConjunto (CompraUnidad)
-VALUES (6),
-(7),
-(8),
-(9),
-(10),
-(11);
-
-
-
-
-
-
+INSERT INTO CompraConjunto 
+    (CompraUnidad, FormaPago_Usuario)
+VALUES 
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6);
